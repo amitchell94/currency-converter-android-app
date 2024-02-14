443e7ef1e0f68a7d12e6ae4f2df75e292c29e083
amitchell94 <andymitchell1@live.co.uk>
2024-02-14 12:55:58 +0000

    upgraded gradle version and added API call to convert currency

---
+++ app/src/main/java/com/andytmitchell/currencyconverter/CurrrencyConverterWidgetProvider.java
@@ -0,0 +1,11 @@
+package com.andytmitchell.currencyconverter;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+public class CurrrencyConverterWidgetProvider extends AppWidgetProvider {
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // Perform update on the widget
+    }
+}
--- app/src/main/java/com/andytmitchell/currencyconverter/MainActivity.java
+++ app/src/main/java/com/andytmitchell/currencyconverter/MainActivity.java
@@ -3,6 +3,16 @@ package com.andytmitchell.currencyconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,51 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getCurrencyRate();
+    }
+
+    private void getCurrencyRate() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HttpURLConnection connection = null;
+                try {
+                    URL url = new URL("https://api.exchangerate-api.com/v4/latest/GBP");
+                    connection = (HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("GET");
+                    connection.connect();
+
+                    InputStream inputStream = connection.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                    StringBuilder response = new StringBuilder();
+
+                    String line;
+                    while((line = reader.readLine()) != null) {
+                        response.append(line);
+                    }
+
+                    JSONObject jsonResponse = new JSONObject(response.toString());
+                    JSONObject rates = jsonResponse.getJSONObject("rates");
+                    final double rateToEUR = rates.getDouble("EUR");
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            TextView textView = findViewById(R.id.textView);
+                            textView.setText("GBP to EUR: " + rateToEUR);
+                        }
+                    });
+
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    if (connection != null) {
+                        connection.disconnect();;
+                    }
+                }
+            }
+        }).start();
     }
 }
