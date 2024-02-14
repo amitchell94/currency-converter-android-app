8edbf2a6ad4931d862e82d7c2d37ee8173777e27
amitchell94 <andymitchell1@live.co.uk>
2024-02-14 13:45:58 +0000

    added select currencies for currency conversion

--- app/src/main/java/com/andytmitchell/currencyconverter/MainActivity.java
+++ app/src/main/java/com/andytmitchell/currencyconverter/MainActivity.java
@@ -3,6 +3,10 @@ package com.andytmitchell.currencyconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import org.json.JSONObject;
@@ -11,7 +15,6 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,16 +24,46 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getCurrencyRate();
+        final Spinner spinnerHomeCurrency = findViewById(R.id.spinnerHomeCurrency);
+        final Spinner spinnerTargetCurrency = findViewById(R.id.spinnerTargetCurrency);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.currency_array, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinnerHomeCurrency.setAdapter(adapter);
+        spinnerTargetCurrency.setAdapter(adapter);
+
+        setSpinnerToValue(spinnerHomeCurrency, "GBP");
+        setSpinnerToValue(spinnerTargetCurrency, "EUR");
+
+        getCurrencyRate("GBP", "EUR");
+        AdapterView.OnItemSelectedListener currencySelectedListener = new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String homeCurrency = spinnerHomeCurrency.getSelectedItem().toString();
+                String targetCurrency = spinnerTargetCurrency.getSelectedItem().toString();
+                getCurrencyRate(homeCurrency,targetCurrency);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        };
+
+        spinnerHomeCurrency.setOnItemSelectedListener(currencySelectedListener);
+        spinnerTargetCurrency.setOnItemSelectedListener(currencySelectedListener);
     }
 
-    private void getCurrencyRate() {
+    private void getCurrencyRate(final String homeCurrency, final String targetCurrency) {
         new Thread(new Runnable() {
             @Override
             public void run() {
                 HttpURLConnection connection = null;
                 try {
-                    URL url = new URL("https://api.exchangerate-api.com/v4/latest/GBP");
+                    URL url = new URL("https://api.exchangerate-api.com/v4/latest/" + homeCurrency);
                     connection = (HttpURLConnection) url.openConnection();
                     connection.setRequestMethod("GET");
                     connection.connect();
@@ -46,13 +79,13 @@ public class MainActivity extends AppCompatActivity {
 
                     JSONObject jsonResponse = new JSONObject(response.toString());
                     JSONObject rates = jsonResponse.getJSONObject("rates");
-                    final double rateToEUR = rates.getDouble("EUR");
+                    final double rate = rates.getDouble(targetCurrency);
 
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
                             TextView textView = findViewById(R.id.textView);
-                            textView.setText("GBP to EUR: " + rateToEUR);
+                            textView.setText(homeCurrency + " to " + targetCurrency + ": " + rate);
                         }
                     });
 
@@ -66,5 +99,11 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }).start();
+    }
+
+    private void setSpinnerToValue(Spinner spinner, String value) {
+        ArrayAdapter<CharSequence> adapter = (ArrayAdapter<CharSequence>) spinner.getAdapter();
+        int position = adapter.getPosition(value);
+        spinner.setSelection(position);
     }
 }
